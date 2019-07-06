f7109546fc453baabf9b2078d71f5e98fa6bc2aa
Nikita Krapivin <alienoom@yandex.ru>
2019-07-06 21:13:39 +0500

    Upload first version.
    (it's terrible)

---
+++ GMTGUI/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace GMTGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.GMTDocsLink = new System.Windows.Forms.LinkLabel();
+            this.GMTDiscordLink = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove Game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(354, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Configure Game";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(335, 342);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // GMTDocsLink
+            // 
+            this.GMTDocsLink.AutoSize = true;
+            this.GMTDocsLink.Location = new System.Drawing.Point(354, 303);
+            this.GMTDocsLink.Name = "GMTDocsLink";
+            this.GMTDocsLink.Size = new System.Drawing.Size(136, 13);
+            this.GMTDocsLink.TabIndex = 4;
+            this.GMTDocsLink.TabStop = true;
+            this.GMTDocsLink.Text = "Read GMT documentation!";
+            this.GMTDocsLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GMTDocsLink_LinkClicked);
+            // 
+            // GMTDiscordLink
+            // 
+            this.GMTDiscordLink.AutoSize = true;
+            this.GMTDiscordLink.Location = new System.Drawing.Point(354, 326);
+            this.GMTDiscordLink.Name = "GMTDiscordLink";
+            this.GMTDiscordLink.Size = new System.Drawing.Size(127, 13);
+            this.GMTDiscordLink.TabIndex = 5;
+            this.GMTDiscordLink.TabStop = true;
+            this.GMTDiscordLink.Text = "Visit GMT Discord server!";
+            this.GMTDiscordLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GMTDiscordLink_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(354, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Launch Game";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 365);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.GMTDiscordLink);
+            this.Controls.Add(this.GMTDocsLink);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GMTogether GUI by nkrapivin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.LinkLabel GMTDocsLink;
+        private System.Windows.Forms.LinkLabel GMTDiscordLink;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ GMTGUI/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+// INI config lib
+using System.IO;
+using IniParser;
+using IniParser.Model;
+
+namespace GMTGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Exe files (*.exe)|*.exe";
+            var parser = new FileIniDataParser();
+            if (File.Exists(Application.StartupPath + "\\GMTGUI.ini"))
+            {
+                IniData data = parser.ReadFile("GMTGUI.ini");
+                string GMTLauncherPath = data["Main"]["GMTLauncherPath"];
+                foreach (var key in data["Games"])
+                {
+                    int tempgamescount = listBox1.Items.Count;
+                    listBox1.Items.Insert(tempgamescount, key.Value);
+                }
+            }
+            else
+            {
+                File.CreateText(Application.StartupPath + "\\GMTGUI.ini").Close(); //Make an empty file. Or else IniParser will crash :(
+                IniData data = parser.ReadFile("GMTGUI.ini"); // yes.
+                MessageBox.Show(this,"Looks like it's the first time you're running GMTogether GUI.\nYou need to specify path to GMT-Launcher.exe","Oh no",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel) //ask for GMT-Launcher.exe
+                    return;
+                string GMTLauncherPath = openFileDialog1.FileName;
+                data["Main"]["GMTLauncherPath"] = GMTLauncherPath;
+                parser.WriteFile("GMTGUI.ini", data);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            int tempgamescount = listBox1.Items.Count;
+            listBox1.Items.Insert(tempgamescount, filename);
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("GMTGUI.ini");
+            data["Games"]["Game"+tempgamescount.ToString()] = filename;
+            parser.WriteFile("GMTGUI.ini", data);
+        }
+
+        //Links to cool, dino approved(tm) resources.
+        private void GMTDocsLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://yal.cc/r/19/gmt");
+        }
+
+        private void GMTDiscordLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/4e63T3W");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("GMTGUI.ini");
+            string GMTLauncherPath = data["Main"]["GMTLauncherPath"];
+            if (File.Exists(GMTLauncherPath))
+            {
+                if (File.Exists(listBox1.SelectedItem.ToString()))
+                {
+                    Process.Start(GMTLauncherPath, listBox1.SelectedItem.ToString());
+                }
+                else //game exe does not exist!
+                {
+                    MessageBox.Show(this, "Looks like your game exe stopped existing at the path you chose!\nYou need to specify a new path and try again!", "Oh no", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (openFileDialog1.ShowDialog() == DialogResult.Cancel) //ask for new game exe
+                        return;
+                    string newGamePath = openFileDialog1.FileName;
+                    listBox1.Items[listBox1.SelectedIndex] = newGamePath;
+                    data["Games"]["Game" + listBox1.SelectedIndex.ToString()] = newGamePath;
+                    parser.WriteFile("GMTGUI.ini", data);
+                }
+            }
+            else //for some reason GMT-Launcher.exe doesn't exist
+            {
+                MessageBox.Show(this, "Looks like GMT-Launcher.exe stopped existing at the path you chose.\nYou need to specify a new one and try again!", "Oh no", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel) //ask for GMT-Launcher.exe....... again
+                    return;
+                string NewGMTLauncherPath = openFileDialog1.FileName;
+                data["Main"]["GMTLauncherPath"] = NewGMTLauncherPath; //change gmt launcher path to a new file
+                parser.WriteFile("GMTGUI.ini", data);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //listBox1.SetSelected(listBox1.SelectedIndex, true);
+            // NO!
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedindex = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(selectedindex);
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("GMTGUI.ini");
+            data.Sections.RemoveSection("Games");
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                data["Games"]["Game" + i.ToString()] = listBox1.Items[i].ToString(); //I know it's terrible.
+            }
+            parser.WriteFile("GMTGUI.ini", data);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not yet implemented, sorry.");
+        }
+    }
+}
